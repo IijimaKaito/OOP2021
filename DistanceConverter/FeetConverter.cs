4617517cd8c5ada1c26336f75246bf4fe091f92d
IijimaKaito <32072@ojs.ac.jp>
2021-05-25 10:29:59 +0900

    p50リスト2.6

---
+++ DistanceConverter/FeetConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    class FeetConverter {
+        //フィートからメートルを求める
+        public  double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+        //メートルからフィートを求める
+        public  double MeterToFeet(int meter) {
+            return meter / 0.3048;
+        }
+    }
+}
--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -15,24 +15,19 @@ namespace DistanceConverter {
             }
         }
         private static void PrintMeterToFeatList(int stat,int stop) {
+            FeetConverter converter = new FeetConverter();
             for (int meter = 1; meter <= 10; meter++) {
-                    double feet = MeterToFeet(meter);
+                    double feet = converter.MeterToFeet(meter);
         Console.WriteLine("{0} m = {1:0.0000}ft", meter, feet);
     }
 }
         private static void PrintFeetToMeterList(int stat, int stop) {
+            FeetConverter converter = new FeetConverter();
             for (int feet = 1; feet <= 10; feet++) {
-                double meter = FeetToMeter(feet);
+                double meter = converter.FeetToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000}m", feet, meter);
             }
         }
-        //フィートからメートルを求める
-        static double FeetToMeter(int feet) {
-            return feet * 0.3048;
-        }
-        //メートルからフィートを求める
-        static double MeterToFeet(int meter) {
-            return meter / 0.3048;
-        }
+      
     }
 }
