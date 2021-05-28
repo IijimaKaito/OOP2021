2e3399c2ffd34b6e70e010016d7da9d191555077
IijimaKaito <32072@ojs.ac.jp>
2021-05-28 14:04:45 +0900

    p27リスト2.26_完成版

--- SalesCalcukator/Program.cs
+++ SalesCalcukator/Program.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
-           var sales = new SalesCounter("Sales.csv");
-            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
-            foreach(KeyValuePair<string,int> obj in amountPerStore) {
-                Console.WriteLine("{0}{1}", obj.Key, obj.Value);
+            var sales = new SalesCounter("Sales.csv");
+            
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach(var obj in amountPerStore) {
+                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
             }
         }
     }
