dbd3b6326134ec0850218a3f289d74dd110c1a37
IijimaKaito <32072@ojs.ac.jp>
2021-06-01 10:46:56 +0900

    問題2.3作成

---
+++ Exercise3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+            var sales = new SalesCounter("Sales.csv");
+
+            var amountPerStore = sales.GetPerStoreSales();//店舗別売り上げを求める
+            foreach(var obj in amountPerStore) {
+                Console.WriteLine("{0}{1}", obj.Key, obj.Value);
+            }
+        }
+    }
+}
