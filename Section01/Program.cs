d59ac7fcf7ddda92424f124cdca2d3f1e481e5e0
IijimaKaito <32072@ojs.ac.jp>
2021-06-08 09:35:10 +0900

    p100 問題３-２-４完成

--- Section01/Program.cs
+++ Section01/Program.cs
@@ -8,6 +8,8 @@ namespace Section01 {
     class Program {
         static void Main(string[] args) {
             var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+            var names = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", }; 
+            
             Exercise1_1(numbers);
             Console.WriteLine("-----");
 
@@ -22,7 +24,34 @@ namespace Section01 {
             // 3.1.4
             Exercise1_4(numbers);
 
+            // 3.2.1
+            Exercise2_1(names);
+            Console.WriteLine();
+
+            // 3.2.2
+            Exercise2_2(names);
+            Console.WriteLine();
+
+            // 3.2.3
+            Exercise2_3(names);
+            Console.WriteLine();
+
+            // 3.2.4
+            Exercise2_4(names);
+            Console.WriteLine();
+            //出来た人用
+            Exercise2_5(names);
+            Console.WriteLine();
         }
+        //出来た人問題
+        private static void Exercise2_5(List<string> names) {
+            int count = 0;
+            foreach(var name in names) {
+                count += name.Count(c => char.IsLower(c));
+            }
+            Console.WriteLine(count);
+        }
+
         private static void Exercise1_1(List<int> numbers) {
             var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
 
@@ -49,5 +78,24 @@ namespace Section01 {
                 Console.WriteLine(num);
             }
         }
+        private static void Exercise2_1(List<string> names) {
+            Console.WriteLine("都市名を入力＞");
+            var line = Console.ReadLine();//入力データ取り込み
+            var index = names.FindIndex(s => s == line);
+            Console.WriteLine(index);
+        }
+        private static void Exercise2_2(List<string> names) {
+            var count = names.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
+        }
+        private static void Exercise2_3(List<string> names) {
+            var selected = names.Where(s => s.Contains('o')).ToArray();
+            foreach (var name in selected)
+                Console.WriteLine(name);
+        }
+        private static void Exercise2_4(List<string> names) {
+            var selected = names.Where(s => s.StartsWith("B"));
+        }
     }
+    
 }
