02e1c7f0d4aadfb4a3679bf8734839db40e22534
IijimaKaito <32072@ojs.ac.jp>
2021-06-10 14:05:27 +0900

    p129 4.44

--- Section1/Program.cs
+++ Section1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Section01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,30 @@ using System.Threading.Tasks;
 namespace Section1 {
     class Program {
         static void Main(string[] args) {
+            var Session = new Dictionary<string, object>();
+            Session["MyProduct"] = null;
+            var product = Session["MyProduct"] as Product;
+            if (product == null) {
+
+                Console.WriteLine("productが取得できなかった");
+            } else {
+
+                Console.WriteLine("producが取得できた");
+            }
         }
+
+        private static Product GetProductA() {
+            Sale sale = new Sale();
+            sale.Product = new Product();
+            sale = null;
+            return sale?.Product;//null条件演算子
+        }
+    }
+
+    class Sale {
+        public string ShopName { get; set; } = "abcde";
+        public int Amount { get; set; } = 12340;
+        public Product Product { get; set; }
+
     }
 }
