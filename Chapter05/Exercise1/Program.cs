b32ccfbfd375fc83fe0687ae264d5ff7718654a6
IijimaKaito <32072@ojs.ac.jp>
2021-06-11 15:53:05 +0900

    p159 Chapter05　問題5.1 完成

---
+++ Chapter05/Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+            //文字列が等しいか等しくないか比較する
+            if (String.Compare(s1, s2, true) == 0) {
+                Console.WriteLine("一致しています");
+            } else {
+                Console.WriteLine("一致していません");
+            }
+
+        }
+    }
+}
