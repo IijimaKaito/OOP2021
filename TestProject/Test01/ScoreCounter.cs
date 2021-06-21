2595ec87496f7849a7d6de4e9db8ce93ab4d16de
IijimaKaito <32072@ojs.ac.jp>
2021-06-21 14:30:21 +0900

    設問１完成

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using static Test01.Student;
 
 namespace Test01 {
     class ScoreCounter {
@@ -7,33 +8,61 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
 
 
 
-            
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+            List<ScoreCounter> score= new List<ScoreCounter>();//学生データリストオブジェクトを生成
+            string[] lines = File.ReadAllLines(filePath);//ファイルから一気に読み込む
+            foreach (string line in lines) {//読み込んだ行の数だけ繰り返す
+                string[] items = line.Split(',');
+                int height;
+                if (int.TryParse(Subject,out height)) {
+
+                } else {
+
+                }
+                ScoreCounter scoreCounter = new Student { 
+                    
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                score.Add(scoreCounter);
+            }
+            return score;
 
 
 
 
 
 
-            
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _score) {
+                if (dict.ContainsKey(sale.Name))
+                    //コレクションへ名前を登録
+                    dict[sale.Name] += sale.Amount;
+                else
+                    //コレクションへ店舗を登録
+                    dict[sale.ShopName] = sale.Amount;
 
-
-
-
-
-            
+            }
+            return dict;
         }
+
+
+
+
+
+    }
     }
 }
--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,7 +1,10 @@
 ﻿namespace Test01 {
     class Student {
 
+        public string Name { get; set; }
+        public int Subject{ get; set; }
+    public int Score{ get; set; }
 
 
-    }
+}
 }
