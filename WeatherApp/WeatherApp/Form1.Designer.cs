6f43a95ab8b6267fd6ebd42ef07d4e2cef0beb89
海翔 飯嶋 <32072@ojs.ac.jp>
2021-09-03 15:59:44 +0900

    演習２途中

---
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace WeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.code = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ボタン";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // code
+            // 
+            this.code.FormattingEnabled = true;
+            this.code.Items.AddRange(new object[] {
+            "前橋",
+            "みなかみ",
+            "水戸",
+            "宇都宮"});
+            this.code.Location = new System.Drawing.Point(69, 93);
+            this.code.Name = "code";
+            this.code.Size = new System.Drawing.Size(201, 23);
+            this.code.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.code);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox code;
+    }
+}
+
