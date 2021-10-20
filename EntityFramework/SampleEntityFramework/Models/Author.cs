f00a500d74a514c33e36daa3d10811814b8345b1
IijimaKaito <32072@ojs.ac.jp>
2021-10-20 15:58:22 +0900

    Chapter13演習問題途中

---
+++ EntityFramework/SampleEntityFramework/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleEntityFramework.Models
+{
+    //エンティティクラス
+    public class Author
+    {
+        public int Id { get; set; }//主キー
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Gender { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
