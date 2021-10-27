538bcea228ecb96a745418d70cf4f230061603bc
IijimaKaito <32072@ojs.ac.jp>
2021-10-27 14:25:14 +0900

    AddresBook

---
+++ ADO.NET/AddressBook/infosys202111DataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace AddressBook
+{
+
+
+    partial class infosys202111DataSet
+    {
+        partial class PersonDataTable
+        {
+        }
+    }
+}
+
+namespace AddressBook.infosys202111DataSetTableAdapters {
+    
+    
+    public partial class PersonTableAdapter {
+    }
+}
