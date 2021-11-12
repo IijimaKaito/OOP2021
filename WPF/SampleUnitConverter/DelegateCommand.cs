2592a8524678005f0eb7cf8057e7a3989f3a16fa
IijimaKaito <32072@ojs.ac.jp>
2021-11-12 13:12:00 +0900

    SampleUnitConverter 途中

---
+++ WPF/SampleUnitConverter/DelegateCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleUnitConverter
+{
+    class DelegateCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public DelegateCommand(Action execute)
+            : this(execute, () => true) { }
+
+
+        public DelegateCommand(Action execute,
+            Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+        public bool CanExecute(object parameter)
+        {
+                return this.canExecute();
+        }
+        public event EventHandler CanExecuteChanged;
+    }
+}
